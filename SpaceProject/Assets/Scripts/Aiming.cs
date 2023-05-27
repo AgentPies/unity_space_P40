2623b38ee1da4d0d200788b7be9cd39010a2a683
AgentPies <julekziel@gmail.com>
2023-05-27 12:50:14 +0200

    Add door animation

---
+++ SpaceProject/Assets/Scripts/Aiming.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Aiming : MonoBehaviour
+{
+    private Camera mainCamera;
+
+
+    private void Start()
+    {
+        // Cache the camera, Camera.main is an expensive operation.
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Aim();
+    }
+
+    private void Aim()
+    {
+        var (success, position) = GetMousePosition();
+        if (success)
+        {
+            // Calculate the direction
+            var direction = position - transform.position;
+
+            // You might want to delete this line.
+            // Ignore the height difference.
+            direction.y = 0;
+
+            // Make the transform look in the direction.
+            transform.forward = direction;
+        }
+    }
+
+    private (bool success, Vector3 position) GetMousePosition()
+    {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity))
+        {
+            // The Raycast hit something, return with the position.
+            return (success: true, position: hitInfo.point);
+        }
+        else
+        {
+            // The Raycast did not hit anything.
+            return (success: false, position: Vector3.zero);
+        }
+    }
+}
--- SpaceProject/Assets/Scripts/WFC.cs
+++ SpaceProject/Assets/Scripts/WFC.cs
@@ -12,16 +12,17 @@ public class WFC : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     //DODAĆ: Przypadki w rogach
+    //DODAĆ: Usuwanie pokoi gdy się nie stykają z niczym (dodać bool czy jest połączony z czymś a jak ma false to usunąć na końcu)
     {        
-        int x = 0;
+        float x = 0;
         GameObject[] instantiatedRooms = new GameObject[width * height];
         
         int currentRoom = 0;
-        int roomsInColumn  = height / 20;
-        int roomsInRow = width / 20;
+        int roomsInColumn  = Mathf.CeilToInt(height / 19.5f);
+        int roomsInRow = Mathf.CeilToInt(width / 20);
         while (x < width)
         {
-            int z = 0;
+            float z = 0;
             while (z < height)
             {   
                 int breakCounter = 0;
@@ -111,7 +112,7 @@ public class WFC : MonoBehaviour
                 }
                 z = z + 20;
             }
-            x = x + 20;
+            x = x + 19.5f;
         }
     }       
 }
