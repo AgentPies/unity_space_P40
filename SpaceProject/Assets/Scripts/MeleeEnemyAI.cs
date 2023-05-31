862fea3356b8f4d49c117cebeb59708652fedca1
MarcinWiel <mar.wielewicki@gmail.com>
2023-05-31 23:19:12 +0200

    Melee Enemy AI
    Added melee enemy, room box collider

---
+++ SpaceProject/Assets/Scripts/MeleeEnemyAI.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class MeleeEnemyAI : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float detectionRange = 10f;
+    public Transform target;
+    public Collider roomCollider;
+
+    private bool isInRoom;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        isInRoom = false;
+    }
+
+    private void Update()
+    {
+        // Check if the target is within detection range and in the same room
+        if (Vector3.Distance(transform.position, target.position) <= detectionRange && isInRoom)
+        {
+            // Move towards the target
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+
+        if (Vector3.Distance(transform.position, target.position) <= detectionRange && isInRoom)
+        {
+            // Move towards the target
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+
+        Debug.Log("Distance: " + Vector3.Distance(transform.position, target.position));
+        Debug.Log("In Room: " + isInRoom);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the enemy enters a room
+        if (other == roomCollider)
+        {
+            isInRoom = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the enemy exits a room
+        if (other == roomCollider)
+        {
+            isInRoom = false;
+        }
+    }
+}
